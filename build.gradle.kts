33e26b4a9d7466ed469fc2aa18119f6ba53e5505
Haouabarkallah <haouabarkallah@gmail.com>
2026-04-21 17:19:53 +0200

    feat(dashboard): add analytics and charts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // Top-level build file
 
 plugins {
-    id("com.android.application") version "9.0.1" apply false
-    id("com.android.library") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
-    id("com.google.dagger.hilt.android") version "2.59.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
 
 tasks.register("clean", Delete::class) {
